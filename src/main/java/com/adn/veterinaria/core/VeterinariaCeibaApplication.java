1c417a13b7dc18985316bc69fd249b0ac2c7bebf
Mario Ruiz <mario.ruiz@ceiba.com.co>
2020-12-01 22:06:22 -0500

    initial commit

---
+++ src/main/java/com/adn/veterinaria/core/VeterinariaCeibaApplication.java
@@ -0,0 +1,13 @@
+package com.adn.veterinaria.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinariaCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinariaCeibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adn/veterinaria/core/VeterinariaCeibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adn.veterinaria.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinariaCeibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
