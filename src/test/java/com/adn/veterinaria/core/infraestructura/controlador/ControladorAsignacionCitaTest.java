d288414327a29f3be239a8766a0671593cd05abf
Mario Ruiz <mario.ruiz@ceiba.com.co>
2020-12-03 16:06:28 -0500

    menor: cambio en controlador cita test para paso en jenkins

--- src/test/java/com/adn/veterinaria/core/infraestructura/controlador/ControladorAsignacionCitaTest.java
+++ src/test/java/com/adn/veterinaria/core/infraestructura/controlador/ControladorAsignacionCitaTest.java
@@ -2,6 +2,7 @@ package com.adn.veterinaria.core.infraestructura.controlador;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +14,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.adn.veterinaria.core.aplicacion.comando.ComandoCitaVeterinaria;
+import com.adn.veterinaria.core.aplicacion.comando.ComandoMascota;
+import com.adn.veterinaria.core.aplicacion.comando.ComandoPersona;
+import com.adn.veterinaria.core.aplicacion.comando.ComandoTipoCita;
 import com.adn.veterinaria.core.testdatabuilder.CitaVeterinariaTestDataBuilder;
+import com.adn.veterinaria.core.testdatabuilder.MascotaTestDataBuilder;
+import com.adn.veterinaria.core.testdatabuilder.PersonaTestDataBuilder;
+import com.adn.veterinaria.core.testdatabuilder.TipoCitaTestDataBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(SpringExtension.class)
@@ -28,15 +35,53 @@ class ControladorAsignacionCitaTest {
 	private ObjectMapper objectMapper;
 
 	@Test
+	@BeforeEach
+	void before() throws Exception {
+		long randomId = Math.round((Math.random() * 100));
+		ComandoPersona comandoResponsable = new PersonaTestDataBuilder()
+				.conIdentificacion("1017231" + randomId)
+				.buildComando();
+
+		mvc.perform(MockMvcRequestBuilders.post("/operador/responsables")
+				.content(objectMapper.writeValueAsString(comandoResponsable))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated());
+
+		ComandoMascota comandoMascota = new MascotaTestDataBuilder().buildComando();
+
+		mvc.perform(MockMvcRequestBuilders.post("/operador/mascotas")
+				.content(objectMapper.writeValueAsString(comandoMascota))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated());
+
+		ComandoPersona comandoVeterinario = new PersonaTestDataBuilder()
+				.conIdentificacion("875455" + randomId)
+				.buildComando();
+
+		mvc.perform(MockMvcRequestBuilders.post("/operador/veterinarios")
+				.content(objectMapper.writeValueAsString(comandoVeterinario))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated());
+
+		ComandoTipoCita comandoTipoCita = new TipoCitaTestDataBuilder().buildComando();
+
+		mvc.perform(MockMvcRequestBuilders.post("/operador/tipo-citas")
+				.content(objectMapper.writeValueAsString(comandoTipoCita))
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated());
+	}
+
+	@Test
 	void asignarCita() throws Exception {
 
 		ComandoCitaVeterinaria comandoCita = new CitaVeterinariaTestDataBuilder().buildComando();
 
-		mvc.perform(MockMvcRequestBuilders
-				.post("/citas/asignar")
-				.content(objectMapper.writeValueAsString(comandoCita))
-				.contentType(MediaType.APPLICATION_JSON)
-				.accept(MediaType.APPLICATION_JSON))
+		mvc.perform(MockMvcRequestBuilders.post("/citas/asignar").content(objectMapper.writeValueAsString(comandoCita))
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isCreated());
 	}
 }
